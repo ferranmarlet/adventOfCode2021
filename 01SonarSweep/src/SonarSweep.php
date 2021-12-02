1496fe58ef1c56577d72048814834ff54d49d9ea
Ferran Marlet <ferran.marlet@holaluz.com>
2021-12-02 21:57:13 +0100

    Add logic for empty measurements list

---
+++ 01SonarSweep/src/SonarSweep.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdventOfCode\SonarSweep;
+
+class SonarSweep
+{
+    public $measurements;
+
+    public function countIncreasingDepthVariations(): int
+    {
+        return 0;
+    }
+}
+
