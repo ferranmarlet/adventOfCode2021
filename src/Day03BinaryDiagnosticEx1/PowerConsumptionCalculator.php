2a374696c1a69a4c289430aba209d2dd32d1c210
Ferran Marlet <ferran.marlet@holaluz.com>
2021-12-04 20:19:12 +0100

    Use transposer on power consumption calculator

--- src/Day03BinaryDiagnosticEx1/PowerConsumptionCalculator.php
+++ src/Day03BinaryDiagnosticEx1/PowerConsumptionCalculator.php
@@ -4,50 +4,32 @@ declare(strict_types=1);
 
 namespace AdventOfCode\Day03BinaryDiagnosticEx1;
 
+use AdventOfCode\Day03BinaryDiagnosticEx1\DiagnosticRecordTransposer;
+
 class PowerConsumptionCalculator
 {
-    private array $diagnosticReport;
-    private array $onesOcurrencesByColumn;
+    private array $transposedRecords;
 
     public function __construct(array $diagnosticReport)
     {
-        $this->diagnosticReport = $diagnosticReport;
-        for ($i = 0; $i < count(str_split($diagnosticReport[0])); $i++) {
-            $this->onesOcurrencesByColumn[$i] = 0;
-        }
+        $this->transposedRecords = DiagnosticRecordTransposer::transpose($diagnosticReport);
     }
 
     public function getPowerConsumption(): int
     {
-        foreach ($this->diagnosticReport as $binaryEntry) {
-            $binaryCharacters = str_split($binaryEntry);
+        $binaryGammaRate = '';
+        $binaryEpsilonRate = '';
 
-            for ($i = 0; $i < count($binaryCharacters); $i++) {
-                $this->onesOcurrencesByColumn[$i] += $binaryCharacters[$i];
-            }
-        }
-
-        $binaryGammaRate = $this->calculateBinaryGammaRate();
-        $binaryEpsilonRate = $this->calculateBinaryEpsilonRate($binaryGammaRate);
-        return (bindec($binaryGammaRate) * bindec($binaryEpsilonRate));
-    }
-
-    private function calculateBinaryGammaRate(): string
-    {
-        $gammaRate = '';
-        for ($i = 0; $i < count($this->onesOcurrencesByColumn); $i++) {
-            if ($this->onesOcurrencesByColumn[$i] > count($this->diagnosticReport)/2) {
-                $gammaRate .= '1';
+        foreach ($this->transposedRecords as $record) {
+            if (substr_count($record, '1') > strlen($record)/2) {
+                $binaryGammaRate .= '1';
+                $binaryEpsilonRate .= '0';
             } else {
-                $gammaRate .= '0';
+                $binaryGammaRate .= '0';
+                $binaryEpsilonRate .= '1';
             }
         }
 
-        return $gammaRate;
-    }
-
-    private function calculateBinaryEpsilonRate(string $binaryGammaRate): string
-    {
-        return strtr($binaryGammaRate, '01', '10');
+        return (bindec($binaryGammaRate) * bindec($binaryEpsilonRate));
     }
 }
