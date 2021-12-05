c563f41ab6fe98144bdbb38b99cfb2811854159d
Ferran Marlet <ferran.marlet@holaluz.com>
2021-12-05 16:27:20 +0100

    Implement logic on LifeSupportRatingCalculator

--- src/Day03BinaryDiagnosticEx2/LifeSupportRatingCalculator.php
+++ src/Day03BinaryDiagnosticEx2/LifeSupportRatingCalculator.php
@@ -6,4 +6,20 @@ namespace AdventOfCode\Day03BinaryDiagnosticEx2;
 
 class LifeSupportRatingCalculator
 {
+    private array $records;
+
+    public function __construct(array $records)
+    {
+        $this->records = $records;
+    }
+
+    public function execute(): int
+    {
+        $calculateOxygenRating = new CalculateOxygenGeneratorRating();
+        $calculateCO2Rating = new CalculateCO2ScrubberRating();
+        $oxygenBinaryRating = $calculateOxygenRating->execute($this->records);
+        $CO2BinaryRating = $calculateCO2Rating->execute($this->records);
+
+        return bindec($oxygenBinaryRating) * bindec($CO2BinaryRating);
+    }
 }
