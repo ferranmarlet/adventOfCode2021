e69f62effc4154dd010f6ce73022f9e6341358b2
Ferran Marlet <ferran.marlet@holaluz.com>
2021-12-05 17:13:20 +0100

    Add simple logic for a winning board sized 1x1

--- src/Day04GiantSquidEx1/BingoCheatCalculator.php
+++ src/Day04GiantSquidEx1/BingoCheatCalculator.php
@@ -6,12 +6,17 @@ namespace AdventOfCode\Day04GiantSquidEx1;
 
 class BingoCheatCalculator
 {
+    private array $drawnNumbers;
+    private array $boards;
+
     public function __construct(array $drawnNumbers, array $boards)
     {
-        
+        $this->drawnNumbers = $drawnNumbers;
+        $this->boards = $boards;
     }
 
     public function cheat(): int
     {
+        return 0;
     }
 }
