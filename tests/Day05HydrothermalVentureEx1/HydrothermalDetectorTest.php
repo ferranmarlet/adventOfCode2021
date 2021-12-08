b461165725504fed98e9cc8a68bdf217744975ae
Ferran Marlet <ferran.marlet@holaluz.com>
2021-12-08 16:20:24 +0100

    Add test for multiple vents and straight lines

--- tests/Day05HydrothermalVentureEx1/HydrothermalDetectorTest.php
+++ tests/Day05HydrothermalVentureEx1/HydrothermalDetectorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use AdventOfCode\Day05HydrothermalVentureEx1\CoordinatesToArrayTransformer;
 use AdventOfCode\Day05HydrothermalVentureEx1\HydrothermalDetector;
 use PHPUnit\Framework\TestCase;
 
@@ -41,5 +42,25 @@ class HydrothermalDetectorTest extends TestCase
         ];
         $sut = new HydrothermalDetector($map);
         self::assertEquals(1, $sut->getDangerousSpotCount());
+    }
+
+    /** @test */
+    public function shouldDetectMultipledangerousSpotsOnVerticalAndHorizontalLinesOnly(): void
+    {
+        $coordinateStrings = [
+            '0,9 -> 5,9',
+            '8,0 -> 0,8',
+            '9,4 -> 3,4',
+            '2,2 -> 2,1',
+            '7,0 -> 7,4',
+            '6,4 -> 2,0',
+            '0,9 -> 2,9',
+            '3,4 -> 1,4',
+            '0,0 -> 8,8',
+            '5,5 -> 8,2'
+        ];
+        $map = CoordinatesToArrayTransformer::transform($coordinateStrings);
+        $sut = new HydrothermalDetector($map);
+        self::assertEquals(5, $sut->getDangerousSpotCount());
     }
 }
